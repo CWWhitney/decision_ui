d2fd66fd6acd40721e3177f5dce329eabaa910e9
Thomas Low <code@thomaslow.com>
2026-03-30 13:05:45 +0200

    Update staging run script.

--- deployment/staging/bin/run.sh
+++ deployment/staging/bin/run.sh
@@ -6,5 +6,6 @@ podman run \
     --rm -it \
     -v ./data:/root/workspace/code/server/data:Z \
     -p 8080:8080 \
-    -e DSUI_SECRET=default_secret \
+    -e DSUI_ACCESS_TOKEN_SECRET=my_secret \
+    -e DSUI_REFRESH_TOKEN_SECRET=my_secret \
     localhost/decision-support-ui/server:latest
